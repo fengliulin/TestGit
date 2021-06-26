e7501bfceea6b610add409178250db52bbf43a2b
fengliulin-home <menin@163.com>
2021-06-26 22:05:12 +0800

    dfd

---
+++ ChenghengUserAccountSonToRole.java
@@ -0,0 +1,67 @@
+package com.sample;
+
+
+public class ChenghengUserAccountSonToRole {
+
+  private long userAccountSonToRoleId;
+  private long userAccountId;
+  private long userAccountSonId;
+  private long userRoleId;
+  private java.sql.Timestamp createDatetime;
+  private java.sql.Timestamp updateDatetime;
+
+
+  public long getUserAccountSonToRoleId() {
+    return userAccountSonToRoleId;
+  }
+
+  public void setUserAccountSonToRoleId(long userAccountSonToRoleId) {
+    this.userAccountSonToRoleId = userAccountSonToRoleId;
+  }
+
+
+  public long getUserAccountId() {
+    return userAccountId;
+  }
+
+  public void setUserAccountId(long userAccountId) {
+    this.userAccountId = userAccountId;
+  }
+
+
+  public long getUserAccountSonId() {
+    return userAccountSonId;
+  }
+
+  public void setUserAccountSonId(long userAccountSonId) {
+    this.userAccountSonId = userAccountSonId;
+  }
+
+
+  public long getUserRoleId() {
+    return userRoleId;
+  }
+
+  public void setUserRoleId(long userRoleId) {
+    this.userRoleId = userRoleId;
+  }
+
+
+  public java.sql.Timestamp getCreateDatetime() {
+    return createDatetime;
+  }
+
+  public void setCreateDatetime(java.sql.Timestamp createDatetime) {
+    this.createDatetime = createDatetime;
+  }
+
+
+  public java.sql.Timestamp getUpdateDatetime() {
+    return updateDatetime;
+  }
+
+  public void setUpdateDatetime(java.sql.Timestamp updateDatetime) {
+    this.updateDatetime = updateDatetime;
+  }
+
+}
